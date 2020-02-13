05461b8e6172749de4dd7a9ed5bd61d188a77b10
Alword <anzer987@yandex.ru>
2020-02-13 22:28:21 +0300

    feature positive compression test

---
+++ SimpleStringСompression.BusinessLogicTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleStringСompression.BusinessLogicTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleStringСompression.BusinessLogicTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e053f2cc-29d2-4cda-b306-6db9261fd09a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleStringСompression.BusinessLogicTests/SimpleTextCompressorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleStringCompression.BusinessLogic;
+
+namespace SimpleStringСompression.BusinessLogicTests
+{
+    [TestClass]
+    public class SimpleTextCompressorTest
+    {
+        [TestMethod]
+        public void CompressSimpleString()
+        {
+            // arrange
+            string text = "aaab";
+            string expected = "(3)ab";
+            SimpleTextCompressor simpleTextCompressor = new SimpleTextCompressor();
+
+            // act
+            string actual = simpleTextCompressor.Compress(text);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
